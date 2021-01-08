447a73fd3470caf80d464fd133aeab6f1ccd4844
Hans <romanghans@gmail.com>
2021-01-07 21:26:12 -0700

    touching ups...

---
+++ src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="post-list">
+		<div v-for="post in posts" :key="post">
+			<SinglePost :post="post" />
+		</div>
+	</div>
+</template>
+
+<script>
+import SinglePost from './SinglePost.vue'
+import { onMounted, onUnmounted } from 'vue'
+export default {
+	props:['posts'],
+	components:{SinglePost},
+	setup(props){
+		onMounted(() => {
+			console.log('component mounted');
+			
+		})
+		onUnmounted(()=> {
+			console.log('component unmounted');
+		})
+	}
+}
+</script>
+
+<style>
+
+</style>
